bdacbc0c3ce419bf90cf71ff7f9cefd324f7ab70
Kilian DUCLOS <97612798+BonelessGod@users.noreply.github.com>
2022-03-18 10:33:38 +0100

    Update shrinkHeader.js

--- script/shrinkHeader.js
+++ script/shrinkHeader.js
@@ -1,16 +1,25 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() {scrollFunction()};
 
+let photoCSSAttribute = document.getElementById("photo"), 
+    photoOriginalWidth = getComputedStyle(photo, null).width,
+    photoOriginalHeight = getComputedStyle(photo, null).height;
+
+let photoScrolledWidth = photoOriginalWidth.replace(/px/, "") * 0.7 + "px";
+let photoScrolledHeight = photoOriginalHeight.replace(/px/, "") * 0.6 + "px";
+
 function scrollFunction() {
   if (document.documentElement.scrollTop > 30) {
-    document.getElementById("headerShrink").style.fontSize = "small";
-    document.getElementById("headerShrink").style.height = "50%";
-    document.getElementById("photo").style.width = "20%"
+
+    document.getElementById("photo").style.width = photoScrolledWidth;
+    document.getElementById("photo_info").style.height = photoScrolledHeight;
     document.getElementById("titleName").style.fontSize = "large";
+
   } else {
-    document.getElementById("headerShrink").style.fontSize = "large";
-    document.getElementById("headerShrink").style.height = "100%";
-    document.getElementById("photo").style.width = "35%"
+
+    document.getElementById("photo").style.width = photoOriginalWidth;
+    document.getElementById("photo_info").style.height = photoOriginalHeight;
     document.getElementById("titleName").style.fontSize = "xx-large";
+
   }
 }
